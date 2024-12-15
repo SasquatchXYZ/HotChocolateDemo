05ba9cce343d2c88d73627602f429ed6aa59ed12
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-15 16:37:48 -0500

    Add GraphQL services

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using HotChocolateDemo.GraphQL;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    // Returns an `IRequestExecutorBuilder`, which has many extension methods
+    // that can be used to configure the GraphQL schema
+    .AddGraphQLServer()
+    // We are specifying the Query type that should be exposed by our GraphQL server
+    .AddQueryType<Query>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
